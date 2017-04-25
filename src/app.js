21a4d0b5101040e3a88f816806847a5252fc9744
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2017-04-25 01:13:55 -0300

    add lexer

---
+++ src/app.js
@@ -0,0 +1,62 @@
+(function() {
+    'use strict';
+
+    var lex = function(input) {
+        var isOperator = function(c) { return /[+\-*\/\^%=(),]/.test(c); },
+            isDigit = function(c) { return /[0-9]/.test(c); },
+            isWhiteSpace = function(c) { return /\s/.test(c); },
+            isIdentifier = function(c) { return typeof c === "string" && !isOperator(c) && !isDigit(c) && !isWhiteSpace(c); };
+
+        var tokens = [], c, i = 0;
+        var advance = function() { return c = input[++i]; };
+        var addToken = function(type, value) {
+            tokens.push({
+                type: type,
+                value: value
+            });
+        };
+
+        while(i < input.length) {
+            c = input[i];
+
+            if(isWhiteSpace(c)) {
+                advance()
+            } else if(isOperator(c)) {
+                addToken(c);
+                advance(0.)
+            } else if(isDigit(c)) {
+                var num = c;
+
+                while(isDigit(advance())) {
+                    num += c;
+                }
+
+                if(c === ".") {
+                    do num += c; while(isDigit(advance()));
+                }
+
+                num = parseFloat(num);
+
+                if(!isFinite(num)) {
+                    throw "Number is too large or too small for a 64-bit double.";
+                }
+
+                addToken("number", num);
+            } else if(isIdentifier(c)) {
+                var idn = c;
+
+                while(isIdentifier(advance())) {
+                    idn += c;
+                }
+
+                addToken("identifier", idn);
+            } else {
+                throw "Unrecognized token.";
+            }
+        }
+
+        addToken("(end)");
+        
+        return tokens;
+    };
+})();
